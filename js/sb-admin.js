79903a142e1b02b77320851c7fc5c9c688cb2610
Pengcheng Zou <zoupc1989@gmail.com>
2017-04-10 18:32:05 +0800

    Add Table

--- js/sb-admin.js
+++ js/sb-admin.js
@@ -1,3 +1,19 @@
 $(function() {
-    
+	$('.navbar .angle').click(function() {
+		var direction = $('i', this).hasClass('fa-angle-left') ? 'left' : 'right';
+
+		if (direction === 'left') {
+			$('.side-bar').hide()
+
+			$('#wrapper, .navbar-inverse').css({'padding-left': '0'})
+
+			$('i', this).removeClass('fa-angle-left').addClass('fa-angle-right')
+		} else {
+			$('.side-bar').show()
+
+			$('#wrapper, .navbar-inverse').css({'padding-left': '250px'})
+
+			$('i', this).removeClass('fa-angle-right').addClass('fa-angle-left')
+		}
+	});
 });
